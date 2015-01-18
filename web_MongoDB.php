f07d94ff644bf93baa73bf0d323c9cc509af1111
Mar <mar.munoz2@gmail.com>
2015-01-18 23:59:19 +0100

    Update web_MongoDB.php

--- web_MongoDB.php
+++ web_MongoDB.php
@@ -1,13 +1,28 @@
 # Código fuente para conectar la BBDD de MongoDB, y ver su contenido desde una página simple realizada en Php.
 # Este sería el contenido del fichero index.php
-<?php
-	 //conectamos con mongo, suponiendo que lo haces en local (localhost)
-	$m = new MongoClient("mongodb://testUser:testPass@localhost");     -- pongo datos de prueba, no lo publico
+<html>
+ <head>
+  <title>Conexion MongoDB</title>
+ </head>
+ <body>
+  <?php echo "<p>A continuacion podemos visualizar los tuits que hemos capturado</p>"; ?>
+
+  <?php
+	$m = new MongoClient( "mongodb://servidor" ); -- oculto la info del servidor
 	$db = $m->selectDB('twitter'); // nombre de la base de datos
-	$db->authenticate("admin", " My password ");                       -- pongo datos de prueba, no lo publico
 	$info = $db->selectCollection("datos_twitter"); //seleccionar la colección
 	$msgs = $info->find(); //busca y filtra la colección
+	$i = 0;
 	foreach ($msgs as $msg) { //recorre la colección en busca de la columna 'text'
-		echo "TEXTOS:".$msg['text']."\n";
+		echo $i;
+		echo " - ";
+		$text = $msg['text'];
+//		var_dump($text[$i]);
+		echo $text[$i];
+
+		echo "<br>";
+		$i = $i + 1;
 	}
-?>
+  ?>
+ </body>
+</html>
